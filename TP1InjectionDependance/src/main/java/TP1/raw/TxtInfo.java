3e6e9921618f19c72e7e45bddc8b6c42d3d4d21f
Mikael DAUTREY <mikael.dautrey@isitix.com>
2016-09-28 15:45:50 +0200

    Update TxtInfo.java

--- TP1InjectionDependance/src/main/java/TP1/raw/TxtInfo.java
+++ TP1InjectionDependance/src/main/java/TP1/raw/TxtInfo.java
@@ -4,9 +4,11 @@ package TP1.raw;
  * Created by mdautrey on 26/09/16.
  */
 public class TxtInfo extends FileInfo {
-
+    public TxtInfo(String fileName){
+        setFileName(fileName);
+    }
     @Override
     public int getNRows() {
-        // A COMPLETER
+        return 123;
     }
 }
