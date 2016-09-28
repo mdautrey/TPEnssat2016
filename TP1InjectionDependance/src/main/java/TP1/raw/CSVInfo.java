3111a50c640b5743c70bd879c359d1f380ba34ea
Mikael DAUTREY <mikael.dautrey@isitix.com>
2016-09-28 15:39:16 +0200

    Update CSVInfo.java

--- TP1InjectionDependance/src/main/java/TP1/raw/CSVInfo.java
+++ TP1InjectionDependance/src/main/java/TP1/raw/CSVInfo.java
@@ -4,6 +4,11 @@ package TP1.raw;
  * Created by mdautrey on 26/09/16.
  */
 public class CSVInfo extends FileInfo {
+    
+    public CSVInfo(String fileName){
+        setFileName(fileName);
+    }
+   
     @Override
     public int getNRows() {
         // A COMPLETER
